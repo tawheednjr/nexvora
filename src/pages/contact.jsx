e570bc490114895d03ef810ea1f7991b2ef332b0
Tawheed <tawheednjr298@gmail.com>
2026-07-28 21:39:08 +0530

    clear some bugs

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -34,6 +34,7 @@ const handleSubmit = async (e) => {
   try {
     
     // const response = await fetch("http://localhost:5000/send-email",
+    // const response = await fetch("http://localhost:5000/send-email",
     const response = await fetch("https://nexvora-backend-gamma.vercel.app/api/send-email",
       {
         method: "POST",
